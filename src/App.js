ea20f38e00833d71c66e0dc5a04d6e676142cdcf
njihiamark <njihiamark@gmail.com>
2020-09-18 02:22:39 +0300

    render user table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React,  {useState } from "react";
+import React, { useState, useEffect } from "react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import Users from "./components/Users";
+import Axios from "axios";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -10,11 +11,30 @@ function App() {
   const [totalUsersInvited, setTotalUsersInvited] = useState(0);
   const [calculatingInvitedUsers, setCalculatingInvitedUsers] = useState(true);
   const [totalPublishedCampaigns, setTotalPublishedCampaigns] = useState(0);
-  const [calculatingPublishedCampaigns, setCalculatingPublishedCampaigns] = useState(true);
+  const [
+    calculatingPublishedCampaigns,
+    setCalculatingPublishedCampaigns,
+  ] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      //fetch data
+      const result = await Axios("http://localhost:3000/example_data.json");
+      //sort data by created_at newest first
+      const newest = await result.data.slice().sort((a, b) => {
+        let c = new Date(a.created_at);
+        let d = new Date(b.created_at);
+        return d - c;
+      });
+
+      setLastUserAddedAt(newest[0].name);
+      setUsers(newest);
+    })();
+  }, []);
 
   return (
     <div className="Main">
-      <Users />
+      <Users data={users}/ >
     </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,24 +1,32 @@
 import React from "react";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Card, Image } from "semantic-ui-react";
 
-const Users = () => (
+const Users = ({ data }) => (
   <Card.Group>
-    <Card>
-      <Card.Content>
-        <Image
-          floated="right"
-          size="mini"
-          src="https://d1ts43dypk8bqh.cloudfront.net/v1/avatars/eb38d065-c587-402f-aa17-135c754db73a"
-        />
-        <Card.Header>Aaron Cody</Card.Header>
-        <Card.Meta color="black">Owner</Card.Meta>
-        <Card.Description>
-          <span><strong>Invited users:</strong> 3</span>&nbsp;&nbsp;
-          <span><strong>Published campaigns:</strong> 0</span>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>aaron@trychameleon.com</Card.Content>
-    </Card>
+    {data.map((user) => (
+      <Card key={user.id}>
+        <Card.Content>
+          <Image floated="right" size="mini" src={user.cached_avatar} />
+          <Card.Header>{user.name}</Card.Header>
+          <Card.Meta>{user.style}</Card.Meta>
+          <Card.Meta>
+            Created at:&nbsp;
+            {user.created_at}
+          </Card.Meta>
+          <Card.Description>
+            <span>
+              <strong>Invited users:</strong> {user.stats.invited_users_count}
+            </span>
+            &nbsp;&nbsp;
+            <span>
+              <strong>Published campaigns: </strong>
+              {user.stats.published_campaigns_count || 0}
+            </span>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>{user.email}</Card.Content>
+      </Card>
+    ))}
   </Card.Group>
 );
 
