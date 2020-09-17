7e312fa0ac07c61fa94f65e7206948009cbfc243
njihiamark <njihiamark@gmail.com>
2020-09-18 00:55:14 +0300

    setup initial state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React,  {useState } from "react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import Users from "./components/Users";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [usersLoading, setUsersLoading] = useState(true);
+  const [lastUserAddedAt, setLastUserAddedAt] = useState("");
+  const [totalUsersInvited, setTotalUsersInvited] = useState(0);
+  const [calculatingInvitedUsers, setCalculatingInvitedUsers] = useState(true);
+  const [totalPublishedCampaigns, setTotalPublishedCampaigns] = useState(0);
+  const [calculatingPublishedCampaigns, setCalculatingPublishedCampaigns] = useState(true);
+
   return (
     <div className="Main">
       <Users />
